a47a7c213f2e659984506f4a2af268f3fb691740
Eric Tang <67912192+EZ-Tang@users.noreply.github.com>
2022-07-11 19:46:57 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='HW14Package',
+    version='1.0.0',
+    url='https://github.com/EZ-Tang/HW14/',
+    author='Eric cTang',
+    author_email='ezt4q@virginia.edu',
+    description='Homework 14 for a data science class.',
+)
